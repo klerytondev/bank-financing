f12247e6ce32425446502f52a63aac3d28205981
Kleryton <kleryton.dev@gmail.com>
2022-04-18 19:32:54 -0300

    created project

---
+++ src/test/java/com/br/bankfinancing/BankFinancingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.bankfinancing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankFinancingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
