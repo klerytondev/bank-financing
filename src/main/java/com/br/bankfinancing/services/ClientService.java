f8800733ce19c76a7538ee8f29ee5afbfec18e4c
Kleryton <kleryton.dev@gmail.com>
2022-04-30 19:46:13 -0300

    implemented class ClienteService and created ObjetoNaoEncontradoException

--- src/main/java/com/br/bankfinancing/services/ClientService.java
+++ src/main/java/com/br/bankfinancing/services/ClientService.java
@@ -1,5 +1,8 @@
 package com.br.bankfinancing.services;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +12,12 @@ import com.br.bankfinancing.models.ClientModel;
 import com.br.bankfinancing.repositories.ClientRepository;
 import com.br.bankfinancing.requestDto.ClientRequestDto;
 import com.br.bankfinancing.services.exceptions.ConflictDeDadosException;
+import com.br.bankfinancing.services.exceptions.ObjetoNaoEncontradoException;
+
+import br.com.kleryton.bankingsystem.models.AccountModel;
 
 public class ClientService {
-	
+
 	@Autowired
 	ClientRepository clientRepository;
 
@@ -21,20 +27,28 @@ public class ClientService {
 		ClientModel clientModel = convertDtoToModel(clienteRequestDto);
 		try {
 			clientRepository.save(clientModel);
-			//TODO implementar exception
+			// TODO implementar exception
 		} catch (DataIntegrityViolationException e) {
 			throw new ConflictDeDadosException("");
 		}
 		return clientModel;
 	}
 
-	// Coverte um request DTO em client
-	private ClientModel convertDtoToModel(ClientRequestDto clienteRequestDto) {
-		// TODO Auto-generated method stub
-		return null;
+	public Object findAll() {
+		// Verifica se existe clientes no banco, caso contrario retorna exception
+				if (clientRepository.findAll().isEmpty()) {
+					throw new ObjetoNaoEncontradoException("Clientes not found!");
+				}
+				// Salva clientes existentes no banco de dados em uma lista de clientes
+				List<ClientModel> clientModelsList = new ArrayList<>();
+				for (ClientModel clientModel : clientRepository.findAll()) {
+					clientModelsList.add(clientModel);
+				}
+				return clientModelsList;
 	}
 
-	public Object findAll() {
+	// Coverte um request DTO em client
+	private ClientModel convertDtoToModel(ClientRequestDto clienteRequestDto) {
 		// TODO Auto-generated method stub
 		return null;
 	}
