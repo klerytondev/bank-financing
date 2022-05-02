af0f3fa062cd606bb5b310d232789b98e55d8f6d
Kleryton <kleryton.dev@gmail.com>
2022-05-02 20:00:48 -0300

    implemented Enum StatusClient

--- src/main/java/com/br/bankfinancing/models/enums/StatusClient.java
+++ src/main/java/com/br/bankfinancing/models/enums/StatusClient.java
@@ -1,7 +1,25 @@
 package com.br.bankfinancing.models.enums;
 
+import javax.validation.constraints.Pattern;
+
 public enum StatusClient {
-	
-	ATIVO, INATIVO;
+
+	ATIVO("ativo"), INATIVO("intativo");
+
+	// @regex validar os status do finaciamento
+	@Pattern(regexp = "ATIVO|INATIVO")
+	private String statusClient;
+
+	private StatusClient(String statusClient) {
+		this.statusClient = statusClient;
+	}
+
+	public String getStatusClient() {
+		return statusClient;
+	}
+
+	public void setStatusClient(String statusClient) {
+		this.statusClient = statusClient;
+	}
 
 }
--- src/main/java/com/br/bankfinancing/requestDto/FinancingRequestDto.java
+++ src/main/java/com/br/bankfinancing/requestDto/FinancingRequestDto.java
@@ -4,8 +4,6 @@ import javax.persistence.Column;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.PositiveOrZero;
 
-import org.hibernate.validator.constraints.Length;
-
 import com.br.bankfinancing.models.enums.StatusFinancing;
 import com.br.bankfinancing.models.enums.TypeFinancing;
 
@@ -23,11 +21,11 @@ public class FinancingRequestDto {
 	@NotEmpty(message = "{campo.meses.obrigatorio}")
 	private int meses;
 	
-	@NotEmpty(message = "{campo.typeFinancing.obrigatorio}")
 	private TypeFinancing typeFinancing;
-	@Column(nullable = false)
+	
 	private StatusFinancing statusFinancing;
-	@Column(nullable = false)
+	
+	@PositiveOrZero(message = "{campo.positiveorzero.postivo}")
 	private double inputValueFinancing;
 
 }
