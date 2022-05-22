cd26c9451e2d5c7222156f8a0eb875407370d8c2
Kleryton <kleryton.dev@gmail.com>
2022-05-22 18:18:49 -0300

    Implemented class ClientResponseDto

--- src/main/java/com/br/bankfinancing/responseDto/ClienteResponseDto.java
+++ src/main/java/com/br/bankfinancing/responseDto/ClienteResponseDto.java
@@ -1,5 +1,55 @@
 package com.br.bankfinancing.responseDto;
 
+import java.util.Date;
+import java.util.Set;
+
+import com.br.bankfinancing.models.ClientModel;
+import com.br.bankfinancing.models.FinancingModel;
+import com.br.bankfinancing.models.enums.StatusClient;
+
 public class ClienteResponseDto {
+
+	private Long id;
+	private String name;
+	private String email;
+	private StatusClient statusClient;
+	private Date opdateData;
+	private Set<FinancingModel> financingModels;
+
+	public ClienteResponseDto() {
+	}
+
+	public ClienteResponseDto(ClientModel clientModel) {
+		this.id = clientModel.getId();
+		this.name = clientModel.getName();
+		this.email = clientModel.getEmail();
+		this.statusClient = clientModel.getStatus();
+		this.opdateData = clientModel.getUpdateDate();
+		this.financingModels = clientModel.getFinancingModels();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public StatusClient getStatusClient() {
+		return statusClient;
+	}
+
+	public Date getOpdateData() {
+		return opdateData;
+	}
+
+	public Set<FinancingModel> getFinancingModels() {
+		return financingModels;
+	}
 
 }
